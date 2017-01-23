0a55b6358b28817e555516b7cfcba0fdece5e652
Tomoe Inaba <tomoe@martop.jp>
2017-01-23 10:45:49 +0900

    first commit

---
+++ CrossBorderBan.Common/NativeMethods.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CrossBorderBan.Common
+{
+    static class NativeMethods
+    {
+        /// <summary>
+        /// Delegate mouse hook.
+        /// </summary>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        public delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+        
+        public const int WH_MOUSE_LL = 14;
+
+        /// <summary>
+        /// Number of messages.
+        /// </summary>
+        public enum MouseMessages
+        {
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_MOUSEMOVE = 0x0200,
+            WM_MOUSEWHEEL = 0x020A,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205
+        }
+
+        /// <summary>
+        /// Mouse point.
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        /// <summary>
+        /// Hooked mouse info.
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MouseHookStruct
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        /// <summary>
+        /// Setting hook procedure.
+        /// </summary>
+        /// <param name="idHook"></param>
+        /// <param name="lpfn"></param>
+        /// <param name="hMod"></param>
+        /// <param name="dwThreadId"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr SetWindowsHookEx(int idHook,
+                LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        /// <summary>
+        /// Unhook procedure.
+        /// </summary>
+        /// <param name="hhk"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        /// <summary>
+        /// Calling next hook.
+        /// </summary>
+        /// <param name="hhk"></param>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        /// <summary>
+        /// Getting module handle.
+        /// </summary>
+        /// <param name="lpModuleName"></param>
+        /// <returns></returns>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
---
+++ CrossBorderBan.Common/RangeMouse.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace CrossBorderBan.Common
+{
+    /// <summary>
+    /// Start and stop of the cross-border ban.
+    /// </summary>
+    public static class RangeMouse
+    {
+
+        /// <summary>
+        /// Hook Id.
+        /// </summary>
+        private static IntPtr _hookId = IntPtr.Zero;
+
+        /// <summary>
+        /// Target screen.
+        /// </summary>
+        private static Screen _targetScreen;
+
+        /// <summary>
+        /// Clipping mouse range rectangle.
+        /// </summary>
+        private static Rectangle _mouseRangeRectangle;
+
+        /// <summary>
+        /// Callback mothod.
+        /// </summary>
+        private static readonly NativeMethods.LowLevelMouseProc HookCallbackProc = HookCallback;
+
+        /// <summary>
+        /// Setting Grobal Hook;
+        /// </summary>
+        /// <param name="proc"></param>
+        /// <returns></returns>
+        private static IntPtr SetGrobalHook(NativeMethods.LowLevelMouseProc proc)
+        {
+            using (var curProcess = Process.GetCurrentProcess())
+            using (var curModule = curProcess.MainModule)
+            {
+
+                return NativeMethods.SetWindowsHookEx(NativeMethods.WH_MOUSE_LL, proc,
+                    NativeMethods.GetModuleHandle(curModule.ModuleName), 0);
+
+            }
+        }
+
+        /// <summary>
+        /// Unhook mouse.
+        /// </summary>
+        /// <param name="hookId"></param>
+        private static bool Unhook(IntPtr hookId)
+        {
+            return NativeMethods.UnhookWindowsHookEx(hookId);
+
+        }
+
+        /// <summary>
+        /// Getting mouse range rectangle
+        /// </summary>
+        /// <returns></returns>
+        private static Rectangle GetMouseRangeRectangle(Screen screen)
+        {
+            return screen.Bounds;
+        }
+
+        /// <summary>
+        /// Callback mouse hook.
+        /// </summary>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        internal static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)NativeMethods.MouseMessages.WM_MOUSEMOVE)
+            {
+                var lparam =
+               ((NativeMethods.MouseHookStruct)
+                   Marshal.PtrToStructure(lParam, typeof(NativeMethods.MouseHookStruct)));
+                var point = lparam.pt;
+
+
+                if (!(point.x >= _mouseRangeRectangle.Location.X &&
+                      point.x <= _mouseRangeRectangle.Location.X + _mouseRangeRectangle.Size.Width &&
+                      point.y >= _mouseRangeRectangle.Location.Y &&
+                      point.y <= _mouseRangeRectangle.Location.Y + _mouseRangeRectangle.Size.Height))
+                {
+                    Cursor.Clip = _mouseRangeRectangle;
+                }
+                else
+                {
+                    Cursor.Clip = Rectangle.Empty;
+                }
+            }
+
+            return NativeMethods.CallNextHookEx(_hookId, nCode, wParam, lParam);
+        }
+
+        /// <summary>
+        /// Initialize and start.
+        /// </summary>
+        public static void Start(Screen screen)
+        {
+            if (_hookId != IntPtr.Zero)
+            {
+                if (_targetScreen != null && !_targetScreen.Equals(screen))
+                {
+                    End();
+                    _targetScreen = null;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            _mouseRangeRectangle = GetMouseRangeRectangle(screen);
+
+            Cursor.Clip = _mouseRangeRectangle;
+            _hookId = SetGrobalHook(HookCallbackProc);
+        }
+
+
+        /// <summary>
+        /// stop and post-processing.
+        /// </summary>
+        public static void End()
+        {
+            Cursor.Clip = Rectangle.Empty;
+            if (Unhook(_hookId))
+            {
+                _hookId = IntPtr.Zero;
+            }
+#if DEBUG
+            if (_hookId != IntPtr.Zero)
+                Console.WriteLine("failed unhook！");
+#endif
+        }
+    }
+}
